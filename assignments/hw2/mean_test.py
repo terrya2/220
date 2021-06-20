a1cdf27e8b3bbb82063de9e28fba6560e4f1429e
pabaier <baierpa@g.cofc.edu>
2021-06-19 20:54:27 -0400

    test with api call

--- assignments/hw2/mean_test.py
+++ assignments/hw2/mean_test.py
@@ -4,28 +4,41 @@ import random
 import requests
 from tests import code_style
 from hw2 import mean
+from tests import api_service
+
 
 total = 0
 global_points = 6
 sub_points = 2
-code_style_points = 20
+code_style_points = 12
 
 class TestClass:
 
     def test_output(self, monkeypatch, capfd):
-        global total
-        global global_points
-        global sub_points
-        failed = 0
         inputs = [
             (['4', '10', '5', '2', '5'],{'rms_average': 6.205, 'harmonic_mean': 4.0, 'geometric_mean': 4.729}),
             (['1', '1'], {'rms_average': 1.0, 'harmonic_mean': 1.0, 'geometric_mean': 1.0}),
             (['10', '1', '2','3','4','5','6','7','8','9','10'], {'rms_average': 6.205, 'harmonic_mean': 3.414, 'geometric_mean': 4.529}),
-            (['5', '5','5','5','5','5'], {'rms_average': 5.0, 'harmonic_mean': 5.0, 'geometric_mean': 5.0}),
-
+            (['5', '5','5','5','5','5'], {'rms_average': 5.0, 'harmonic_mean': 5.0, 'geometric_mean': 5.0})
         ]
+        self.run_test(inputs, 'static tests', monkeypatch, capfd)
+
+    def test_api(self, monkeypatch, capfd):
+        data = self.make_test_data(9)
+        response = api_service.test(data, 'hw2')
+        answers = json.loads(response.text)
+        test_data = self.convert_test_data(data, answers)
+        self.run_test(test_data, 'api tests', monkeypatch, capfd)
+
+    @staticmethod
+    def run_test(data, test_type, monkeypatch, capfd):
+        global total
+        global global_points
+        global sub_points
+        failed = 0
+
         outputs = []
-        for inp in inputs:
+        for inp in data:
             userIn = inp[0]
             i = iter(userIn)
             monkeypatch.setattr('builtins.input', lambda inputMessage: next(i))
@@ -34,9 +47,9 @@ class TestClass:
             output = captured.out.strip().split('\n')
             outputs.append({'rms_average': float(output[0]), 'harmonic_mean': float(output[1]), 'geometric_mean': float(output[2])})
 
-        print('\n\n============================== static tests start ===============================\n')
+        print(f'\n\n============================== {test_type} start ===============================\n')
         for index, actual in enumerate(outputs):
-            expected = inputs[index][1]
+            expected = data[index][1]
             test_results = {'rms_average': actual['rms_average'] == expected['rms_average'],
                             'harmonic_mean': actual['harmonic_mean'] == expected['harmonic_mean'],
                             'geometric_mean': actual['geometric_mean'] == expected['geometric_mean']
@@ -65,6 +78,31 @@ class TestClass:
         if failed == 1:
             noun = 'test'
         print(f'\n============================== {failed} {noun} failed ===============================\n')
+
+    @staticmethod
+    def convert_test_data(data, response):
+        inputs = []
+        for index, test in enumerate(response):
+            input_values = list(map(str, data[index]))
+            input_values.insert(0,str(len(data[index])))
+            output_values = {
+                'rms_average': test['rmsAvg'],
+                'harmonic_mean': test['harmonicMean'],
+                'geometric_mean': test['geometricMean']
+            }
+            inputs.append((input_values, output_values))
+        return inputs
+
+    @staticmethod
+    def make_test_data(num):
+        data = []
+        for i in range(0,num):
+            nums = random.randint(1,30)
+            set_nums = []
+            for i in range(0,nums):
+                set_nums.append(random.randint(1,100))
+            data.append(set_nums)
+        return data
 
     def test_linting(self):
         global code_style_points
