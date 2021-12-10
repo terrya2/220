486df97252146182a9ac3a7cea71f83363e06a06
pabaier <baierpa@g.cofc.edu>
2021-12-10 10:53:56 -0500

    remove pytest from hw1

--- assignments/hw1/hello_test.py
+++ assignments/hw1/hello_test.py
@@ -2,17 +2,19 @@ from hw1 import hello
 from tests.test_framework import *
 
 
-class TestClass:
-
-    def test_input(self, capfd):
-        builder = TestBuilder("Hello World!", 'hello.py', linter_points=20, default_test_points=70)
-
-        hello.main()
-        captured = capfd.readouterr()
-        actual = captured.out.strip()
-        expected = "hello, world!"
-
+def main():
+    builder = TestBuilder("Hello World!", 'hello.py', linter_points=20, default_test_points=70)
+    output, result, error = getIO(hello.main)
+    expected = "hello, world!"
+    if error:
+        test = Test('print hello, world!', f'error: {error}', expected)
+    else:
+        actual = output[0]
         test = Test('print hello, world!', actual, expected)
-        builder.add_items(test)
+    builder.add_items(test)
 
-        builder.run()
+    builder.run()
+
+
+if __name__ == '__main__':
+    main()
