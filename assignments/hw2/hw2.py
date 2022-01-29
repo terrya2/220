0199299bf66ef61820002a73994bf6ba84e4d783
terrya2 <terrya2@g.cofc.edu>
2022-01-28 22:34:38 -0500

    Completed hw2

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,36 +1,48 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
+Name: Autumn Terry
+hw2.py
+Creating codes that allow users to do arithmetic by giving input and producing output.
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 import math
-
+# dot notation
 
 def sum_of_threes():
-    pass
+    upper = eval(input("enter the upper bound: "))
+    for i in range(upper):
+        sum = i % upper
+    print("sum of threes is", sum)
 
 
 def multiplication_table():
-    pass
-
+    for i in range (1,11):
+        for j in range (1,11):
+            print(i * j, end='\t')
+        print()
 
 def triangle_area():
-    pass
-
+    side_a = eval(input("enter length of side a: "))
+    side_b = eval(input("enter length of side b: "))
+    side_c = eval(input("enter length of side c: "))
+    s = (side_a + side_b + side_c) / 2
+    area = math.sqrt(s * (s - side_a) * (s - side_b) * (s - side_c))
+    print("area is: ", area)
 
 def sum_squares():
-    pass
-
+    lower = eval(input("enter lower range: "))
+    upper = eval(input("enter upper range: "))
+    sum = 0
+    for i in range(lower,upper, 1):
+        sum = (lower ^ 2) * (i ^ 2) * (upper ^ 2)
+    sum_squares = sum                                  
+    print("sum of squares is: ",sum_squares)
 
 def power():
-    pass
-
+    base = eval(input("enter base: "))
+    exponent = eval(input("enter exponent: "))
+    total = base ^ (exponent)
+    print("The answer is: ", total)
 
 if __name__ == '__main__':
     pass
