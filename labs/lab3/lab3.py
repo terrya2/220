e37b189600136970cdc7dfa2c9f8c094f586b6a5
terrya2 <terrya2@g.cofc.edu>
2022-02-03 16:15:23 -0500

    Completed lab3

--- labs/lab3/lab3.py
+++ labs/lab3/lab3.py
@@ -7,18 +7,18 @@ I certify that this assignment is entirely my own work.
 
 def traffic():
     roads = eval(input("How many roads were surveyed?: "))
-    total_vehicles = sum
+    total_vehicles = 0
     for road in range(roads):
         sum = 0
         print("How many days was road", road + 1,"surveyed?: " )
         days = eval(input(""))
         for traffic in range(days):
-            print("\t", "How many cars traveled on day", traffic + 1, "?: " )
+            print("\tHow many cars traveled on day", traffic + 1, "?: " )
             total_cars = eval(input(""))
             sum = sum + total_cars
+            total_vehicles = total_vehicles + total_cars
         avg = sum / days
         print(road + 1, "average vehicles per day: ", avg)
-    total_vehicles = total_vehicles
     avg_vehicles = total_vehicles / roads
     print("Total number of vehicles traveled on all roads: ", total_vehicles)
     print("Average number of vehicles per road: ", avg_vehicles)
