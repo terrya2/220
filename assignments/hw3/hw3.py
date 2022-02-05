009c704cb7a9651a21f93f0b13696821e3871539
terrya2 <terrya2@g.cofc.edu>
2022-02-04 21:31:46 -0500

    Completed hw3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,50 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
+Autumn Terry
+hw3.py
+Create programs that can solve problems by using loops, inputs, and outputs.
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 
-
 def average():
-    pass
+    num_grades = eval(input("how many grades will you enter?: "))
+    sum = 0
+    for i in range(num_grades):
+        grade_val = eval(input("enter grade: "))
+        sum =  sum + grade_val
+    avg = sum / num_grades
+    print("average is ",avg)
 
 
 def tip_jar():
-    pass
+    sum = 0
+    for i in range(5):
+        donation = eval(input("how much would you like to donate?: "))
+        sum = sum + donation
+    print("total tips: ", sum)
 
 
 def newton():
-    pass
+    number = eval(input("what number do you want to square root?: "))
+    approx = eval(input("how many times should we improve the approximation?: "))
+    ans = (approx + (number / approx)) / (2)
+    print("the square root is approximately ", ans)
 
 
 def sequence():
-    pass
-
+    sum = 0
+    terms = eval(input("how many terms would you like?: "))
+    for i in range(terms):
+        sum = (i + 1) % 2 + i
+    print (sum)
 
 def pi():
-    pass
+    terms = eval(input("how many terms in the series?: "))
+    sum = 0
+    for i in range(terms):
+      sum = ((i - 1) % 2 + i) * ((i + 1) % 2 + i)
+    print(sum)
+
 
 
 if __name__ == '__main__':
--- assignments/hw3/test.py
+++ assignments/hw3/test.py
@@ -1,3 +1,6 @@
-from tests.hw3 import test
-
-test.main()
+def pi():
+    terms = eval(input("how many terms in the series?: "))
+    sum = 0
+    for i in range(terms):
+      sum = ((i - 1) % 2 + i) * ((i +1) % 2 + i)
+    print(sum)
