4af1ab830f694171352bce67ee0c67c3235ef8ce
pabaier <baierpa@g.cofc.edu>
2022-02-16 17:17:26 -0500

    outline hw6

---
+++ assignments/hw6/hw6.py
@@ -0,0 +1,54 @@
+"""
+Name: <your name goes here – first and last>
+<ProgramName>.py
+
+Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+
+Certification of Authenticity:
+<include one of the following>
+I certify that this assignment is entirely my own work.
+I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+"""
+
+
+def cash_converter():
+    pass
+
+
+def encode():
+    pass
+
+
+def sphere_area(radius):
+    pass
+
+
+def sphere_volume(radius):
+    pass
+
+
+def sum_n(number):
+    pass
+
+
+def sum_n_cubes(number):
+    pass
+
+
+def encode_better():
+    pass
+
+
+if __name__ == '__main__':
+    # cash_converter()
+    # encode()
+    # res = sphere_area(13)
+    # print(res)
+    # res = sphere_volume(13)
+    # print(res)
+    # res = sum_n(100)
+    # print(res)
+    # res = sum_n_cubes(13)
+    # print(res)
+    # encode_better()
+    pass
