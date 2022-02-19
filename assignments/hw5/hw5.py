af1eb8f012ef7fe625a6963d2ca3d08148da3f5d
terrya2 <terrya2@g.cofc.edu>
2022-02-18 19:14:11 -0500

    Completed hw5

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -1,42 +1,73 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
+Autumn Terry
+hw5.py
+Creating codes with the use of string and list methods.
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def name_reverse():
-    pass
+    name = str(input("enter a name (first last): "))
+    name.split(" ")
+    first_name = name[0]
+    last_name = name[1]
+    print(last_name, ",", first_name)
+
+    name_reverse()
+
 
 
 def company_name():
-    pass
+    domain = str(input("enter a domain: "))
+    print(domain.split("."))
 
+    company_name()
 
 def initials():
-    pass
+    num_students = str(input("how many students are in the class? "))
+    sum = 0
+    for student in range (num_students):
+        sum = sum + num_students
+        name_student = str(input("what is the name of student", num_students + 1))
+        print(name_student[0])
 
+    initials()
 
 def names():
-    pass
+    list_names = str(input("enter a list of names: "))
+    print(list_names[0])
+
+    names()
 
 
 def thirds():
-    pass
+   num_sentences = str(input("enter the number of sentences: "))
+   sum = 0
+   for sentences in range(num_sentences):
+    sum = sum + num_sentences
+    sentence = str(input("enter sentence", sentences + 1))
+    ans = sentence[0:3]
+    print(ans)
 
+    thirds()
 
 def word_average():
-    pass
+    words = 0
+    sentence = str(input("enter a sentence: "))
+    for words in range (sentence):
+        sum = words + 1
+        print(len(sentence) / sum)
+
+    word_average()
 
 
 def pig_latin():
-    pass
+    sum = 0
+    text = str(input("enter a sentence to convert to pig latin: "))
+    for word in text():
+        pig =  word[0] + "ay"
+        print(pig)
+    pig_latin()
 
 
 if __name__ == '__main__':
