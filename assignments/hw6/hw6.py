31b6a6ce9d67b8313af21d5f25901cbc8748e5b4
terrya2 <terrya2@g.cofc.edu>
2022-02-24 22:29:51 -0500

    Completed hw6

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,54 +1,63 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
+Autumn Terry
+hw6.py
+Create codes using strings, arguments, and return statements.
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
+import math
 
 def cash_converter():
-    pass
-
+    integer = str(input("enter an integer: "))
+    print("That is $", integer.format(integer), ".00")
 
 def encode():
-    pass
-
+    message = str(input("enter a message: "))
+    key = int(input("enter a key: "))
+    for i in message:
+        a = (ord(i) + key)
+        print(chr(a), end="")
 
 def sphere_area(radius):
-    pass
-
+    return 4 * math.pi * (radius ** 2)
 
 def sphere_volume(radius):
-    pass
-
+    return (4 / 3) * math.pi * (radius ** 3)
 
 def sum_n(number):
-    pass
-
+    sum = 0
+    for i in range(1, number + 1):
+        sum += i
+    return sum
 
 def sum_n_cubes(number):
-    pass
-
+    sum = 0
+    for i in range(1, number + 1):
+        sum += i ** 3
+    return sum
 
 def encode_better():
-    pass
-
+    message = str(input("enter a message: "))
+    key = str(input("enter a key: "))
+    ret = ""
+    for i in range(len(message)):
+        charval = ord(message[i]) - 65
+        keyval = ord(key[i % len(key)]) - 65
+        cipherval = ((charval + keyval) % 57) + 64
+        ciphertext = chr(cipherval)
+        ret += ciphertext
+        print(ret)
 
 if __name__ == '__main__':
-    # cash_converter()
-    # encode()
-    # res = sphere_area(13)
-    # print(res)
-    # res = sphere_volume(13)
-    # print(res)
-    # res = sum_n(100)
-    # print(res)
-    # res = sum_n_cubes(13)
-    # print(res)
-    # encode_better()
+    cash_converter()
+    encode()
+    res = sphere_area(13)
+    print(res)
+    res = sphere_volume(13)
+    print(res)
+    res = sum_n(100)
+    print(res)
+    res = sum_n_cubes(13)
+    print(res)
+    encode_better()
     pass
--- assignments/hw6/test.py
+++ assignments/hw6/test.py
@@ -1,3 +1,11 @@
-from tests.hw6 import test
-
-test.main()
+def encode_better():
+    message = str(input("enter a message: "))
+    key = str(input("enter a key: "))
+    ret = ""
+    for i in range(len(message)):
+        charval = ord(message[i]) - 65
+        keyval = ord(key[i % len(key)]) - 65
+        cipherval = ((charval + keyval) % 57) + 64
+        ciphertext = chr(cipherval)
+        ret += ciphertext
+        print(ret)
