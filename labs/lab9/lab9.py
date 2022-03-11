b0b56dbdfde715de2882d285392665325ccfb2ea
pabaier <baierpa@g.cofc.edu>
2022-03-10 22:11:40 -0500

    outline labs 9-13

---
+++ labs/lab9/lab9.py
@@ -0,0 +1,68 @@
+"""
+Name: <your name goes here – first and last>
+<ProgramName>.py
+"""
+
+
+def build_board():
+    pass
+
+
+def print_board(board):
+    """ prints the values of baord """
+    RED = "\033[1;31m"
+    BLUE = "\033[1;36m"
+    LIGHT_GRAY = "\033[0;37m"
+    reset = "\033[0m"
+    new_board = []
+    for v in board:
+        new_board.append(v)
+    for i in range(len(board)):
+        if str(board[i]).find('x') >= 0:
+            new_board[i] = RED + board[i] + LIGHT_GRAY
+        elif str(board[i]).find('o') >= 0:
+            new_board[i] = BLUE + board[i] + LIGHT_GRAY
+    row_format = ' {0} | {1} | {2} '
+    row_1 = row_format.format(new_board[0], new_board[1], new_board[2])
+    row_2 = row_format.format(new_board[3], new_board[4], new_board[5])
+    row_3 = row_format.format(new_board[6], new_board[7], new_board[8])
+    row_separator = '-' * 11
+    print(LIGHT_GRAY)
+    print(row_1)
+    print(row_separator)
+    print(row_2)
+    print(row_separator)
+    print(row_3)
+    print(reset)
+
+
+def is_legal(board, position):
+    pass
+
+
+def fill_spot(board, position, character):
+    pass
+
+
+def winning_game(board):
+    pass
+
+
+def game_over(board):
+    pass
+
+
+def get_winner(board):
+    pass
+
+
+def play(board):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
