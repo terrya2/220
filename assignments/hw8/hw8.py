c2e84f7ad3ea7d7d189476bbc9a35c1c4a1d38b1
terrya2 <terrya2@g.cofc.edu>
2022-03-18 15:28:41 -0400

    Completed hw8

--- assignments/hw8/hw8.py
+++ assignments/hw8/hw8.py
@@ -1,42 +1,60 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
+Autumn Terry
+hw8.py
+Create programs with accumulations and conditional control structures.
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
-"""
 
+"""
+import math
+from graphics import *
 
 def add_ten(nums):
-    pass
-
+    for i in range(len(nums)):
+        nums[i] += 10
 
 def square_each(nums):
-    pass
+    for i in range (len(nums)):
+        nums[i] = nums[i]*nums[i]
 
 
 def sum_list(nums):
-    pass
-
+    sum = 0
+    for i in nums:
+        sum += i
+    return sum
 
 def to_numbers(nums):
-    pass
-
+    for i in range(len(nums)):
+        nums[i] = float (nums[i])
 
 def sum_of_square(nums):
-    pass
+    numbers = []
+    for i in nums:
+        list = i.split(", ")
+        to_numbers(list)
+        square_each(list)
+        numbers.append(sum_list(list))
+    return numbers
+
 
 
 def starter(weight, wins):
-    pass
+    if 150 <= weight < 160 and wins > 5:
+        return True
+    if weight > 199 or wins > 20:
+        return True
+    else:
+        return False
 
 
 def leap_year(year):
-    pass
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            return False
+        return True1
+    return False
 
 
 def circle_overlap():
@@ -59,8 +77,9 @@ def circle_overlap():
 
 
 def did_overlap(circle_one, circle_two):
-    pass
-
+    if math.sqrt(math.pow(circle_two.getCenter().getX() - circle_one.getCenter().getX(), 2)) + (math.pow(circle_two.getCenter().getY() - circle_one.getCenter().getY(), 2)) <= circle_one.getRadius() + circle_two.getRadius():
+        return True
+    return False
 
 if __name__ == '__main__':
     pass
