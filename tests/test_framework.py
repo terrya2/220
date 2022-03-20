e6a1c19297a831b199a251ccf6d303b98bd8bf3f
pabaier <baierpa@g.cofc.edu>
2022-03-19 23:11:11 -0400

    add delta comp function to test framework

--- tests/test_framework.py
+++ tests/test_framework.py
@@ -148,8 +148,8 @@ class Section(TestItem):
             print(f'{tabs}\tdata:')
             for line in self.group_data:
                 print(f'{tabs}\t\t{line}')
-        grade = score_to_letter(
-            self.earned_points * 100 / self.total_points) if self.letter_grades else f'{self.earned_points}/{self.total_points}'
+        score = 0 if self.total_points == 0 else self.earned_points * 100 / self.total_points
+        grade = score_to_letter(score) if self.letter_grades else f'{self.earned_points}/{self.total_points}'
         end_label = f' {self.name} end: {grade} '
         print('{0}{1:-^70}'.format(tabs, end_label))
 
@@ -190,7 +190,8 @@ class TestBuilder:
             'from.*pathlib': 'no need for the pathlib module. please remove it to continue.',
             '\[.*for.*in.*\]': 'list comprehension is not allowed. please remove it to continue',
             'with.*open': 'please open files using the techniques discussed in class',
-            '\..*write.*(.*)': 'please write to files using the techniques discussed in class'
+            '\..*write.*(.*)': 'please write to files using the techniques discussed in class',
+            'yield': 'we do not cover yield in this class. please remove it to continue'
         }
         self.file_name = file_name
         self.rc_file = '../../.pylintrc'
@@ -505,7 +506,8 @@ def build_IO_section(name, tests, expected, dynamic_tests, test_func, test_all_o
     return section
 
 
-def build_IO_string_section(name, tests, expected, dynamic_tests, test_func, num_inputs=1, test_all_output=False, comp_func=None):
+def build_IO_string_section(name, tests, expected, dynamic_tests, test_func, num_inputs=1, test_all_output=False,
+                            comp_func=None):
     """
     :param name: the name of the test
     :param tests: sequence of test inputs
@@ -560,3 +562,10 @@ def make_random_sentence(words=5, word_min=1, word_max=7):
     for i in range(words):
         sentence.append(get_random_string(word_min, word_max))
     return ' '.join(sentence)
+
+
+def delta_comp_func(error_range):
+    def a(actual, expected):
+        return abs(float(actual) - float(expected)) < error_range
+
+    return a
